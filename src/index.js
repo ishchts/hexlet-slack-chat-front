05e3c52b67b1e375a3ef242683fed8811dae644d
Ilya <i1ya1l@yandex.ru>
2021-07-19 19:54:40 +0300

    step-9

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // @ts-check
 import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
+import Rollbar from 'rollbar';
 import '../assets/application.scss';
 import { render } from 'react-dom';
 import runApp from './init.jsx';
@@ -9,4 +10,14 @@ if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-runApp().then((result) => render(result, document.getElementById('chat')));
+runApp().then((result) => {
+  render(result, document.getElementById('chat'));
+  new Rollbar({
+    accessToken: '4be9d205911447faab8069536e5d5cc9',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    payload: {
+      environment: 'production',
+    },
+  });
+});
